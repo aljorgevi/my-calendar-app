27cd9369ab4e8013691641ab1dfec93df5530347
aljorgevi <jormencar@yahoo.com>
2022-05-27 16:02:21 +0100

    progressing calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,7 +1,12 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { messages } from '../../helpers'
 import moment from 'moment'
 import Navbar from '../ui/Navbar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/es'
+
+moment.locale('es')
 
 const localizer = momentLocalizer(moment)
 const events = [
@@ -14,6 +19,20 @@ const events = [
 ]
 
 const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log({ event, start, end, isSelected })
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <div className='calendar-screen'>
       <Navbar />
@@ -22,6 +41,8 @@ const CalendarScreen = () => {
         events={events}
         startAccessor='start'
         endAccessor='end'
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   )
