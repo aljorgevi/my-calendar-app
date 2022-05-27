89333a0f2e6d0ed99eef1c96383ee9e208500b4a
aljorgevi <jormencar@yahoo.com>
2022-05-27 15:49:52 +0100

    add calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,28 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
 import Navbar from '../ui/Navbar'
+
+const localizer = momentLocalizer(moment)
+const events = [
+  {
+    title: 'All Day Event',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgColor: '#f56954'
+  }
+]
 
 const CalendarScreen = () => {
   return (
-    <div>
+    <div className='calendar-screen'>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+      />
     </div>
   )
 }
