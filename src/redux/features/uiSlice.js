f650d5acda6a3fc88c65daab7ad3d95ef8e44cf3
aljorgevi <jormencar@yahoo.com>
2022-06-01 07:31:29 +0100

    add redux toolkit

--- src/redux/features/uiSlice.js
+++ src/redux/features/uiSlice.js
@@ -1,23 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-/*
-import { types } from '../types/types'
-
-const initialState = {
-  modalOpen: false
-}
-
-export const uiReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case types.uiOpenModal:
-      return {
-        ...state,
-        modalOpen: true
-      }
-    default:
-      return state
-  }
-}
-*/
 
 const initialState = {
 	modalOpen: false
