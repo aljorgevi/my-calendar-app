4b61c2e218d21ef6feb46f180c830595da149334
aljorgevi <jormencar@yahoo.com>
2022-06-19 15:03:47 +0100

    add tests for axios

--- src/tests/api/axios.test.js
+++ src/tests/api/axios.test.js
@@ -1,6 +1,30 @@
+import { customFetch } from '../../api/axios';
+
 describe('testing calendar api', () => {
+	const loginDetails = {
+		email: 'test@test.com',
+		password: 'password'
+	};
 
-  test('should have config by default', () => {
+	test('axios should have config baseURL by default', () => {
+		// Arrange
+		const axiosbaseUrl = customFetch.defaults.baseURL;
+		const envURL = process.env.REACT_APP_API_URL;
 
-  })
-})
+		// Assert
+		expect(axiosbaseUrl).toBe(envURL);
+	});
+
+	test('axios should have token in header', async () => {
+		// Arrange
+		const mockTocken =
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9-gkKcW5R-4_4Jz4F_0U6PQ2CKXOaS0f6l7-NpKD-1sVg';
+		localStorage.setItem('token', mockTocken);
+
+		// Act
+		const res = await customFetch.get('/test', loginDetails);
+
+		// Assert
+		expect(res.config.headers['Authorization']).toBe(`Bearer ${mockTocken}`);
+	});
+});
