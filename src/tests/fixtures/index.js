983066b2a6578636707090fad18156ff41eed82f
aljorgevi <jormencar@yahoo.com>
2022-06-22 19:04:07 +0100

    add calendar mocks

--- src/tests/fixtures/index.js
+++ src/tests/fixtures/index.js
@@ -34,3 +34,37 @@ export const testUserCredentials = {
 	password: 'password',
 	username: 'test'
 };
+
+/* calendar */
+
+export const events = [
+	{
+		id: '1',
+		start: new Date('2022-10-21 13:00:00'),
+		end: new Date('2022-10-21 15:00:00'),
+		title: 'birthday party',
+		notes: 'this is a birthday party'
+	},
+	{
+		id: '2',
+		start: new Date('2022-11-09 13:00:00'),
+		end: new Date('2022-11-09 15:00:00'),
+		title: 'wedding anniversary',
+		notes: 'buy a wedding anniversary gift'
+	}
+];
+
+export const calendarInitialState = {
+	events: [],
+	activeEvents: null
+};
+
+export const calendarWithEventsState = {
+	events: [...events],
+	activeEvent: null
+};
+
+export const calendarWithActiveEventState = {
+	events: [...events],
+	activeEvent: { ...events[0] }
+};
